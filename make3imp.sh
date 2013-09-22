d4df0d8486f0d2384ddee89d99364c72e15c6cef
arif <akasim@zenoss.com>
2013-09-22 07:27:40 -0500

    make 256k

--- make3imp.sh
+++ make3imp.sh
@@ -2,7 +2,7 @@
 for f in *.mp4
 do
  echo "processing $f"
- /sbin/ffmpeg -i "$f"  -vn -b:a 128k -c:a libmp3lame "$f".mp3
+ /sbin/ffmpeg -i "$f"  -vn -b:a 256k -c:a libmp3lame "$f".mp3
  rm "$f"
  mv "$f".mp3 $"/Users/arif/Music/iTunes/iTunes Music/Automatically Add to iTunes/"
 done
